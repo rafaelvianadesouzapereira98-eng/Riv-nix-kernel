e882cecc50202ee091a325cca86f18cb2857f293
EstraldaciaOP <rafaelvianadesouzapereira98@gmail.com>
2026-09-21 16:24:09 -0300

    Create vga.h for VGA control and color definitions
    Add VGA header file with color definitions and functions

---
+++ kernel/vga.h
@@ -0,0 +1,34 @@
+#ifndef VGA_H
+#define VGA_H
+
+// Portas do controlador VGA para controle do cursor
+#define VGA_CTRL_REGISTER 0x3d4
+#define VGA_DATA_REGISTER 0x3d5
+#define VGA_OFFSET_LOW 0x0f
+#define VGA_OFFSET_HIGH 0x0e
+
+// Cores básicas do VGA
+#define VGA_COLOR_BLACK 0
+#define VGA_COLOR_BLUE 1
+#define VGA_COLOR_GREEN 2
+#define VGA_COLOR_CYAN 3
+#define VGA_COLOR_RED 4
+#define VGA_COLOR_MAGENTA 5
+#define VGA_COLOR_BROWN 6
+#define VGA_COLOR_LIGHT_GREY 7
+#define VGA_COLOR_DARK_GREY 8
+#define VGA_COLOR_LIGHT_BLUE 9
+#define VGA_COLOR_LIGHT_GREEN 10
+#define VGA_COLOR_LIGHT_CYAN 11
+#define VGA_COLOR_LIGHT_RED 12
+#define VGA_COLOR_LIGHT_MAGENTA 13
+#define VGA_COLOR_YELLOW 14
+#define VGA_COLOR_WHITE 15
+
+// Funções públicas do VGA
+void vga_init();
+void vga_print_char(char c, int color);
+void vga_print(char* message, int color);
+void vga_clear();
+
+#endif
